1d66ea22760940c0583996143a3e4a0552c66891
Sjaypeter <Aleobed902@gmail.com>
2025-08-15 13:59:28 +0100

    Changes

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,21 +1,19 @@
 import mysql.connector
+try:
+    mydb = mysql.connector.connect(
+        host = "Localhost",
+        user = "root",
+        Password = "09033641853#Sjayp",
+        Database = "mydb"
+    )
 
-alx_book_store = mysql.connector.connect(
-    host = "Localhost",
-    user = "root",
-    Password = "09033641853#Sjayp",
-    Database = "alx_book_store"
-)
-
-mycursor = alx_book_store.cursor()
-mycursor.execute(
-    try:
-        CREATE DATABASE IF NOT EXISTS alx_book_store
-    except mysql.connector.Error:
-        print("Please connect to a server!") 
-)
-print("Database 'alx_book_store' created successfully!")
+    mycursor = mydb.cursor()
+    mycursor.execute("""CREATE DATABASE IF NOT EXISTS alx_book_store""")
+    print("Database 'alx_book_store' created successfully!")
+    
+except mysql.connector.Error:
+    print("Please connect to a server")
 
 mycursor.close()
-alx_book_store.close()
+mydb.close()
 print("Database closed")
